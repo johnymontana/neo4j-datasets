c83b616dbe7d54a6693314ad804a5ad14d736253
johnymontana <lyonwj@gmail.com>
2017-01-28 09:41:25 -0500

    update index

--- trumpworld/src/guide/render.sh
+++ trumpworld/src/guide/render.sh
@@ -9,6 +9,7 @@ $GUIDES/run.sh sna.adoc sna.html +1 "$@"
 $GUIDES/run.sh contracts.adoc contracts.html +1 "$@"
 $GUIDES/run.sh exploratory.adoc exploratory.html +1 "$@"
 $GUIDES/run.sh exercises.adoc exercises.html +1 "$@"
+$GUIDES/run.sh interestingqueries.adoc interesting.html +1 "$@"
 }
 
 if [ "$1" == "publish" ]; then
