1d9b1529f7c9fb1767d65947265387f88fda1ef8
johnymontana <lyonwj@gmail.com>
2019-03-01 07:31:37 -0700

    update got guide

--- got/render.sh
+++ got/render.sh
@@ -6,12 +6,12 @@ $GUIDES/run.sh index.adoc index.html +1 "$@"
 }
 
 if [ "$1" == "publish" ]; then
-	URL=guides.neo4j.com/listings
+	URL=guides.neo4j.com/communitydemo
 	render http://$URL -a csv-url=http://guides.neo4j.com/data/got -a env-training
 	s3cmd put --recursive -P *.html img s3://${URL}/
 	s3cmd put -P index.html s3://${URL}
 
-	URL=guides.neo4j.com/
+	URL=guides.neo4j.com/communitydemo
 	render http://$URL -a env-training -a csv-url=file:///
 	s3cmd put --recursive -P *.html img s3://${URL}/
 	s3cmd put -P index.html s3://${URL}
